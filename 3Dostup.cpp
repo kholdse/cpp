f0efc9cd697b8916dfaeb4335037e63dc5a41a33
Romanych <rusroman21@list.ru>
2025-06-20 17:18:58 +0300

    33

--- 3Dostup.cpp
+++ 3Dostup.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class 
+/* Геттеры и Сеттеры */
+class Point
+{
+    public:
+    int GetX()
+    {
+        return x;
+    }
+    void SetX(int valueX)
+    {
+        x = valueX;
+    }
+    private:
+    int x;
+};
+int main()
+{
+    Point a;
+    a.SetX(5);
+    cout << "\t X=" << a.GetX();
+}
